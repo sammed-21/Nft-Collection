49d2553ca81ec81c89761eff02f43f03f68bed68
sammed-21 <sammed2110@gmail.com>
2023-03-26 17:29:28 +0530

    update metadata url

--- client/pages/index.js
+++ client/pages/index.js
@@ -188,7 +188,7 @@ export default function Home() {
 
       // If user is not connected to the Goerli network, let them know and throw an error
       const { chainId } = await web3Provider.getNetwork();
-      if (chainId !== 5) {
+      if (chainId !== 11155111) {
         window.alert("Change the network to Goerli");
         throw new Error("Change network to Goerli");
       }
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,12 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const INFURA_HTTP_URL = process.env.INFURA_HTTP_URL;
+const INFURA_HTTP_SEPOLIA = process.env.INFURA_HTTP_Sepolia;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli: {
       url: INFURA_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    sepolia: {
+      url: INFURA_HTTP_SEPOLIA,
       accounts: [PRIVATE_KEY],
     },
   },
